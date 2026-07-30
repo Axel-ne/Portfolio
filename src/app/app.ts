2116bb008f9c0000d97090bedd07d024a16ef33b
Axel-ne <axel-neugebauer@gmx.net>
2026-07-30 14:49:31 +0200

    skills-section implement

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { LandingSection } from './components/landing-section/landing-section';
 import { AboutMeSection } from './components/about-me-section/about-me-section';
+import { MySkillsSection } from './components/my-skills-section/my-skills-section';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, LandingSection, AboutMeSection],
+  imports: [RouterOutlet, Header, LandingSection, AboutMeSection, MySkillsSection],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
