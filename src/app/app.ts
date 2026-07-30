c3a0ca088521409f99217a1d900e117aefb886f5
Axel-ne <axel-neugebauer@gmx.net>
2026-07-30 16:53:23 +0200

    portfolio section implemented

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Header } from './components/header/header';
 import { LandingSection } from './components/landing-section/landing-section';
 import { AboutMeSection } from './components/about-me-section/about-me-section';
 import { MySkillsSection } from './components/my-skills-section/my-skills-section';
+import { PortfolioSection } from './components/portfolio-section/portfolio-section';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, LandingSection, AboutMeSection, MySkillsSection],
+  imports: [RouterOutlet, Header, LandingSection, AboutMeSection, MySkillsSection, PortfolioSection],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
