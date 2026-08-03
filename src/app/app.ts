0613996a1bd155e66c6601c025e2d5ff9e3e6ec6
Axel-ne <axel-neugebauer@gmx.net>
2026-08-03 16:12:31 +0200

    fix: reference section

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { LandingSection } from './components/landing-section/landing-section';
 import { AboutMeSection } from './components/about-me-section/about-me-section';
 import { MySkillsSection } from './components/my-skills-section/my-skills-section';
 import { PortfolioSection } from './components/portfolio-section/portfolio-section';
+import { References } from './components/references/references';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, LandingSection, AboutMeSection, MySkillsSection, PortfolioSection],
+  imports: [RouterOutlet, Header, LandingSection, AboutMeSection, MySkillsSection, PortfolioSection, References],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
