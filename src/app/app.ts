cef416d847097a07dccb68f2a9498967617ad108
Axel-ne <axel-neugebauer@gmx.net>
2026-08-03 16:21:30 +0200

    add: contact section

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,11 @@ import { AboutMeSection } from './components/about-me-section/about-me-section';
 import { MySkillsSection } from './components/my-skills-section/my-skills-section';
 import { PortfolioSection } from './components/portfolio-section/portfolio-section';
 import { References } from './components/references/references';
+import { ContactSectionn } from './components/contact-sectionn/contact-sectionn';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, LandingSection, AboutMeSection, MySkillsSection, PortfolioSection, References],
+  imports: [RouterOutlet, Header, LandingSection, AboutMeSection, MySkillsSection, PortfolioSection, References, ContactSectionn],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
