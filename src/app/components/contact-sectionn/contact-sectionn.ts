324a9f29d2bbeb8fabfc61c114882f2abadc4eab
Axel-ne <axel-neugebauer@gmx.net>
2026-08-07 10:14:49 +0200

    feat: Content limitation

--- src/app/components/contact-sectionn/contact-sectionn.ts
+++ src/app/components/contact-sectionn/contact-sectionn.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-sectionn',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './contact-sectionn.html',
   styleUrl: './contact-sectionn.scss',
 })
-export class ContactSectionn {}
+export class ContactSectionn {
+  contactForm = new FormGroup({
+    name: new FormControl('', { validators: [Validators.required, Validators.minLength(3)] }),
+    email: new FormControl('', { validators: [Validators.required, Validators.email] }),
+    message: new FormControl('', Validators.required),
+    privacy: new FormControl(false, Validators.requiredTrue),
+  });
+
+  onSubmit() {
+    this.contactForm.reset();
+  }
+}
