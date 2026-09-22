208569aef1e9fcc57a2a9b7b8d64ad944a343ae7
Axel-ne <axel-neugebauer@gmx.net>
2026-09-22 13:32:51 +0200

    add translation folder

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+
+  constructor(private translate: TranslateService) {}
+
+  changeLanguage(lang: 'de' | 'en') {
+    this.translate.use(lang);
+  }
+
+}
