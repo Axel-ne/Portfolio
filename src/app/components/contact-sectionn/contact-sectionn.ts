93408752a6871f1a9eda7a3d04d9193cd1b09def
Axel-ne <axel-neugebauer@gmx.net>
2026-09-22 16:56:48 +0200

    translate contact section

--- src/app/components/contact-sectionn/contact-sectionn.ts
+++ src/app/components/contact-sectionn/contact-sectionn.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact-sectionn',
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule, TranslatePipe],
   templateUrl: './contact-sectionn.html',
   styleUrl: './contact-sectionn.scss',
 })
 export class ContactSectionn {
+
   contactForm = new FormGroup({
-    name: new FormControl('', { validators: [Validators.required, Validators.minLength(3)] }),
-    email: new FormControl('', { validators: [Validators.required, Validators.email] }),
+    name: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(3)]
+    }),
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email]
+    }),
     message: new FormControl('', Validators.required),
     privacy: new FormControl(false, Validators.requiredTrue),
   });
